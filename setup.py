863ae1d7dbee3a66dc48aa397f33506c49bd7862
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2021-05-20 19:30:18 +0200

    package setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='PyGRPY',
+      version='0.0.1',
+      description='Python port of Generalized Rotne Prager Yakamava tensors',
+      url='https://github.com/RadostW/PyGYPY/',
+      author='Radost Waszkiewicz',
+      author_email='radost.waszkiewicz@gmail.com',
+      long_description=long_description,
+      long_description_content_type='text/markdown',  # This is important!
+      license='GNU GPLv3',
+      packages=['pygrpy'],
+      zip_safe=False)
