ea7b61a78cc207fdc9ac449f01623e1ef2955742
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2022-02-02 15:59:23 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name='PyGRPY',
-      version='0.0.8',
+      version='0.1.0',
       description='Python port of Generalized Rotne Prager Yakamava tensors',
       url='https://github.com/RadostW/PyGRPY/',
       author='Radost Waszkiewicz',
